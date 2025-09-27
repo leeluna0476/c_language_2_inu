837b11ef12613ca722b2fe0e37b1bf72ab1b5bfa
LEE LUNA <seojilee@student.42seoul.kr>
2025-09-28 00:51:23 +0900

    둘 데가 없어서 여기 푸시함

---
+++ special/test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// legal ops: ! ~ & ^ | + << >>
+// max op: 10
+int	pad_upper_bits(int n) {
+	int x = (~0x00) + (!n); // 3
+	x = x << (0x20 + (~n + 1)); // 7
+	return x;
+}
+
+void	print_bin(int x) {
+	for (int i = 31; i >= 0; --i) {
+		printf("%d", (x >> i) & 1);
+	}
+	printf("\n");
+}
+
+int	main(void) {
+	for (int i = 0; i <= 32; ++i) {
+		print_bin(pad_upper_bits(i));
+	}
+
+//	print_bin(0xffffffff + 1);
+	return 0;
+}
